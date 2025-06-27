d32b4155602c73e4ccb8a97936b631fa87d53aaa
Murilo Abreu <murilo.lilo@hotmail.com>
2025-06-26 21:31:02 -0400

    Criação de metodo insert do tipo POST  em ProdutcController

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +35,10 @@ public class ProductController {
 	@GetMapping
 	public Page<ProductDTO> findAll(Pageable pageable ){ //Pageable para lista com paginação
 		return productService.findAll(pageable);
+	}
+	
+	public ProductDTO insert(@RequestBody ProductDTO dto) {
+		return productService.insert(dto);
 	}
 
 }
