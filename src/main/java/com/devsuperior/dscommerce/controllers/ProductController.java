054a36a77cb391a157ec330fdbc63b012a9a1629
Murilo Abreu <murilo.lilo@hotmail.com>
2025-06-30 21:53:21 -0400

    Organização no metodo de insert add boas praticas

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,18 +1,19 @@
 package com.devsuperior.dscommerce.controllers;
 
 
-
-
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
@@ -25,22 +26,32 @@ public class ProductController {
 	private ProductService productService;
 	
 	@GetMapping(value = "/{id}")
-	public ProductDTO findById(@PathVariable Long id) {
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		
-		return productService.findById(id);
-		
-	//	ProductDTO dto = productService.findById(id);
-	//	 return dto;		
+		ProductDTO dto = productService.findById(id);
+		return ResponseEntity.ok(dto);		
 	}
 	
 	@GetMapping
-	public Page<ProductDTO> findAll(Pageable pageable ){ //Pageable para lista com paginação
-		return productService.findAll(pageable);
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable ){ //Pageable para lista com paginação
+		
+		Page<ProductDTO> dto = productService.findAll(pageable);
+		
+		return ResponseEntity.ok(dto);
 	}
 	
 	@PostMapping
-	public ProductDTO insert(@RequestBody ProductDTO dto) {
-		return productService.insert(dto);
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		
+		dto = productService.insert(dto);
+		//Boas praticas, vai informar um link do recuso que foi criado
+		URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
