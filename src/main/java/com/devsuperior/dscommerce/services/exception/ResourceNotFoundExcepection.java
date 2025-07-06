cee96e029323d4ebe994d9b881637e26f5e1d2dc
Murilo Abreu <murilo.lilo@hotmail.com>
2025-07-06 16:34:12 -0400

    Criação de Package exception, e classe de exception personalizada

---
+++ src/main/java/com/devsuperior/dscommerce/services/exception/ResourceNotFoundExcepection.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dscommerce.services.exception;
+
+
+
+public class ResourceNotFoundExcepection extends RuntimeException{
+	
+	public ResourceNotFoundExcepection(String msg) {
+		super(msg);
+	}
+
+}
