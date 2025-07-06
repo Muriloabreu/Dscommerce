1e2a3a92f94a9dee1add8904927e47961c8ae733
Murilo Abreu <murilo.lilo@hotmail.com>
2025-07-06 16:33:11 -0400

    Criação de metedo delete em ProductController

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,15 @@ public class ProductController {
 		
 		 dto = productService.update(id, dto);
 		return ResponseEntity.ok(dto);		
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		
+		 productService.delete(id);
+		 
+		 //retorno código 204 sem corpo
+		return ResponseEntity.noContent().build();		
 	}
 
 }
